502a7aefa82057200f2a0bfa5954a11bdbbf1dc1
Giuseppe Lo Brutto <giuseppe.lobrutto@sparkling18.com>
2016-09-15 16:39:04 +0200

    refactoring loyalty card generator

--- loyaltyCardGenerator/main.go
+++ loyaltyCardGenerator/main.go
@@ -19,15 +19,27 @@ func checkError(err error) {
 	}
 }
 
+// card number producer which writes them to the channel out
+func worker(out chan<- string) {
+	rand.Seed(111)
+	for i := 0; i < BATCH_CARDINALITY; i++ {
+		out <- strconv.Itoa(rand.Intn(MAX_CARD_NUMBER))
+	}
+	close(out)
+}
+
 func main() {
+	const CHANNEL_LENGTH = 1000
+	var channel = make(chan string, CHANNEL_LENGTH)
+
+	go worker(channel)
 	fd, err := os.Create("loyaltyCards.csv")
 	checkError(err)
 	writeBuffer := bufio.NewWriter(fd)
 	defer fd.Close()
 
-	rand.Seed(31)
-	for i := 0; i < BATCH_CARDINALITY; i++ {
-		fmt.Fprintf(writeBuffer, "%016s\n", strconv.Itoa(rand.Intn(MAX_CARD_NUMBER)))
+	for v := range channel {
+		fmt.Fprintf(writeBuffer, "%016s\n", v)
 	}
 	writeBuffer.Flush()
 }
