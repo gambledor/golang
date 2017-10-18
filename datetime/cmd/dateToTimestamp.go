22a7bef4168611e24b26dc2e73e35e3c607366c3
Giuseppe Lo Brutto <giuseppelobrutto@gmail.com>
2017-10-18 16:03:38 +0200

    formatting

--- datetime/cmd/dateToTimestamp.go
+++ datetime/cmd/dateToTimestamp.go
@@ -2,35 +2,34 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "log"
-    "os"
-    "golang/datetime/timeutil"
-    "time"
+	"flag"
+	"fmt"
+	"golang/datetime/timeutil"
+	"log"
+	"os"
+	"time"
 )
-
 
 func main() {
 
-    var dateToConvert string
-    var timezone string
+	var dateToConvert string
+	var timezone string
 
-    shortForm := "02/01/2006 15:04:05" // date composed by standard format constants
+	shortForm := "02/01/2006 15:04:05" // date composed by standard format constants
 
-    flag.StringVar(&dateToConvert, "date", time.Now().Format(shortForm), "The date to convert to timestamp")
-    flag.StringVar(&timezone, "tz", "Europe/Rome", "The timezone")
-    flag.Parse()
+	flag.StringVar(&dateToConvert, "date", time.Now().Format(shortForm), "The date to convert to timestamp")
+	flag.StringVar(&timezone, "tz", "Europe/Rome", "The timezone")
+	flag.Parse()
 
-    if len(dateToConvert) == 0 || len(timezone) == 0 {
-        flag.PrintDefaults()
-        os.Exit(1)
-    }
+	if len(dateToConvert) == 0 || len(timezone) == 0 {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 
-    date, err := timeutil.ToTimestamp(dateToConvert, shortForm, timezone)
-    if err != nil {
-        log.Fatal(err)
-    }
+	date, err := timeutil.ToTimestamp(dateToConvert, shortForm, timezone)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    fmt.Println("Location:", date.Location, "Time:", date.DateInLocation, "Timestamp:", date.Timestamp)
+	fmt.Println("Location:", date.Location, "Time:", date.DateInLocation, "Timestamp:", date.Timestamp)
 }
