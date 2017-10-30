3d85650e54e4c37d23d2b59f30c566598b829528
Giuseppe Lo Brutto <giuseppe.lobrutto@blastingnews.com>
2017-10-30 17:41:37 +0100

    added new funtion now in selected timezone

--- datetime/cmd/dateToTimestamp.go
+++ datetime/cmd/dateToTimestamp.go
@@ -12,12 +12,16 @@ import (
 
 func main() {
 
-	var dateToConvert string
-	var timezone string
+	var (
+		command,
+		dateToConvert,
+		timezone string
+	)
 
 	shortForm := "02/01/2006 15:04:05" // date composed by standard format constants
 
-	flag.StringVar(&dateToConvert, "date", time.Now().Format(shortForm), "The date to convert to timestamp")
+	flag.StringVar(&command, "command", "timestamp", "[timestamp|timezone]")
+	flag.StringVar(&dateToConvert, "date", time.Now().Format(shortForm), "the date to convert to timestamp")
 	flag.StringVar(&timezone, "tz", "Europe/Rome", "The timezone")
 	flag.Parse()
 
@@ -26,10 +30,18 @@ func main() {
 		os.Exit(1)
 	}
 
-	date, err := timeutil.ToTimestamp(dateToConvert, shortForm, timezone)
-	if err != nil {
-		log.Fatal(err)
+	if command == "timestamp" {
+		date, err := timeutil.ToTimestamp(dateToConvert, shortForm, timezone)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("location:", date.Location, "time:", date.DateInLocation, "timestamp:", date.Timestamp)
+	} else if command == "timezone" {
+		date, err := timeutil.NowInTimezone(shortForm, timezone)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("location:", date.Location, "time:", date.DateInLocation, "timestamp:", date.Timestamp)
 	}
 
-	fmt.Println("Location:", date.Location, "Time:", date.DateInLocation, "Timestamp:", date.Timestamp)
 }
--- datetime/timeutil/util.go
+++ datetime/timeutil/util.go
@@ -5,24 +5,36 @@ import (
 	"time"
 )
 
-type MyDate struct {
+type myDate struct {
 	Location       *time.Location
 	DateInLocation time.Time
 	Timestamp      int64
 }
 
 // ToTimestamp converts a dateTime in a formatted dateForm in the timezone
-func ToTimestamp(dateTimeToConvert, dateForm, timezone string) (MyDate, error) {
+func ToTimestamp(dateTimeToConvert, dateForm, timezone string) (myDate, error) {
 	location, err := time.LoadLocation(timezone)
 	if err != nil {
-		return MyDate{}, err
+		return myDate{}, err
 	}
 
 	var convertedDate time.Time
 	convertedDate, err = time.ParseInLocation(dateForm, dateTimeToConvert, location)
 	if err != nil {
-		return MyDate{}, err
+		return myDate{}, err
 	}
 
-	return MyDate{location, convertedDate, convertedDate.Unix()}, nil
+	return myDate{location, convertedDate, convertedDate.Unix()}, nil
+}
+
+// NowInTimezone return the datetime in the timezone
+func NowInTimezone(dateForm, timezone string) (myDate, error) {
+	location, err := time.LoadLocation(timezone)
+	if err != nil {
+		return myDate{}, err
+	}
+
+	now := time.Now().In(location)
+
+	return myDate{location, now, now.Unix()}, nil
 }
